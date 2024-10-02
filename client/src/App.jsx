2d2badca7d5ebc4f2ffa39b76dd25527ad423f06
VITOR VARGAS PARAJARA <v.parajara@labcetec.net>
2024-10-02 16:58:49 -0300

    aaa

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,21 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/views/Home';
 import Login from './components/views/Login';
+import GestaoUsuario from './components/views/GestaoUsuario';
+import ImportCSV from './components/views/ImportCSV';
+import CadastroAula from './components/views/CadastroAula';
 
-
+// https://getbootstrap.com/docs/5.3/getting-started/introduction/
 function App() {
   return (
    <Router>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/login' element={<Login/>} />
+        <Route path='/gestao_usuario' element={<GestaoUsuario/>} />
+        <Route path='/import_csv' element={<ImportCSV/>} />
+        <Route path='/cadastro_aula' element={<CadastroAula/>} />
       </Routes>
-
    </Router>
   );
 }
